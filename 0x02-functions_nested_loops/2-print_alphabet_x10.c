e64c136f098cbf21495889bb430e9830c71f48d8
awelekan <awe_lekan@yahoo.com>
2022-12-13 15:17:42 -0800

    low level engineering

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase
+ * followed by new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+i;
+int j;
+for (i = '0'; i <= '9'; i++)
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+{
+_putchar('\n');
+}
+}
