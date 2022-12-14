1fa79d394793e535721ec45ac845fb1111e68875
awelekan <awe_lekan@yahoo.com>
2022-12-13 16:00:09 -0800

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ for (n = 0; n <= 9; n++)
 _putchar('0');
 for (i = 1; i <= 9; i++)
 {
-result = n * i;
 _putchar(',');
 _putchar(' ');
+result = n * i;
 if (result <= 9)
 _putchar(' ');
 else
-_putchar('0' + (result / 10);
+_putchar('0' + (result / 10));
 _putchar('0' + (result % 10));
 }
 _putchar('\n');
