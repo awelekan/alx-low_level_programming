c75bbcbfa0595e8ee6ca3221116c9e2b5b24db55
awelekan <awe_lekan@yahoo.com>
2022-12-16 09:20:33 -0800

    More functions

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: z
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+_putchar('0' + a);
+}
+_putchar('\n');
+}
