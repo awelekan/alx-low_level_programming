3a4071df52c79354ab4f5b15817acac93aefaf4d
awelekan <awe_lekan@yahoo.com>
2022-12-19 18:39:00 -0800

    pointers returned

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -21,3 +21,20 @@ dest[i] = '\0';
 }
 return (dest);
 }
+/**
+
+ * string_length - finds the length of a string.
+
+ * Return: length of c.
+
+ * @pointer: pointer.
+ */
+int string_length(char *pointer)
+{
+int c = 0;
+while (*(pointer + c) != '\0')
+{
+c++;
+}
+return (c);
+}
