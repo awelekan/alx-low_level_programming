b5c67001e53cc3b725655641696f7797e534da2f
awelekan <awe_lekan@yahoo.com>
2023-01-02 08:33:59 -0800

    pointers 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,24 +4,18 @@
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
+ *
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int n = 0;
-int r;
-while (*s)
+unsigned int i, j;
+for (i = 0; s[i] != '\0'; i++)
 {
-for = 0; accept[r]; r++)
+for (j = 0; accept[j] != s[i]; j++)
 {
-if (*s == accept[r])
-{
-n++;
-break;
+if (accept[j] == '\0')
+return (i);
 }
-else if (accept[r + 1] == '\0')
-return (n);
 }
-s++;
-}
-return (n);
+return (i);
 }
