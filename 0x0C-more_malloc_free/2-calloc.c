9ba50f30ded44a3cba2c0786532d0ef1f8f1db1f
awelekan <awe_lekan@yahoo.com>
2023-01-12 17:41:37 -0800

    Maloc free 1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,12 +17,8 @@ if (nmemb == 0 || size == 0)
 return (NULL);
 p = malloc(nmemb * size);
 if (p == NULL)
-{
 return (NULL);
-}
 for (i = 0; i < (nmemb * size); i++)
-{
-*((char *)(p) + i) = 0;
-}
+*((char *)(p) +i) = 0;
 return (p);
 }
